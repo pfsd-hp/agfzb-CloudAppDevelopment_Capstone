80963cdc79f907cf81d7a0a062ed575b9d2dc32c
pfsd-hp <pfsd.hp@gmail.com>
2023-12-30 13:19:56 -0500

    Adding changes to project

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -31,14 +31,43 @@ def contact(request):
 # Create a `login_request` view to handle sign in request
 # def login_request(request):
 # ...
+def login_request(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('djangoapp:index')
+        else:
+            # Invalid login
+            return render(request, 'djangoapp/login.html', {'message': 'Invalid username or password.'})
+    else:
+        return render(request, 'djangoapp/login.html')
 
 # Create a `logout_request` view to handle sign out request
 # def logout_request(request):
 # ...
+def logout_request(request):
+    logout(request)
+    return redirect('djangoapp:index')
 
 # Create a `registration_request` view to handle sign up request
 # def registration_request(request):
 # ...
+def registration_request(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        try:
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+            login(request, user)
+            return redirect('djangoapp:index')
+        except:
+            return render(request, 'djangoapp/registration.html', {'message': 'Error creating user.'})
+    else:
+        return render(request, 'djangoapp/registration.html')
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
 def get_dealerships(request):
